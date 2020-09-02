16dcdf7a2226371c44f0e7410cfa40aed464ff3e
Kmiet <krzys.kmiecik@gmail.com>
2020-09-02 23:53:03 +0200

    Dockerfile & compose updates

--- services/users/api/rest/handlers.go
+++ services/users/api/rest/handlers.go
@@ -2,10 +2,28 @@ package rest
 
 import (
 	users "github.com/Kmiet/fides/services/users"
+	"github.com/gofiber/fiber"
 )
 
 var userService users.UserService
 
 func InitHandlers(service users.UserService) {
 	userService = service
+}
+
+func fetchUserWithID(ctx *fiber.Ctx) {
+	userID := ctx.Params("id")
+	if email, err := userService.FindUserWithID(userID); err != nil {
+		ctx.Send(ctx.JSON(email))
+	} else {
+		ctx.SendStatus(404)
+	}
+}
+
+func registerNewUser(ctx *fiber.Ctx) {
+	if userID, err := userService.Register("test@test.pl"); err != nil {
+		ctx.Send(ctx.JSON(userID))
+	} else {
+		ctx.SendStatus(500)
+	}
 }
--- services/users/api/rest/router.go
+++ services/users/api/rest/router.go
@@ -3,5 +3,8 @@ package rest
 import "github.com/gofiber/fiber"
 
 func InitRouter(app *fiber.App) {
+	api := app.Group("/users")
 
+	api.Post("/", registerNewUser)
+	api.Get("/:id", fetchUserWithID)
 }
